f99d0aa91455332e6dfc996c8bd414cdfbefb225
Justin <justin@ideal.com>
2022-11-22 02:18:27 -0500

    changes

--- abm.py
+++ abm.py
@@ -1,7 +1,9 @@
+import argparse
 from mesa import Agent
 from mesa import Model 
 from mesa.time import RandomActivation
 import scipy.stats as ss
+import numpy as np
 
 # ctask 1 - immediate
 # ctask 2 - 15 minutes
@@ -12,10 +14,11 @@ import scipy.stats as ss
 
 class Patient(Agent):
 	def __init__(self, unique_id, model, ctask_dist, service_distribution):
-		super().__init__(unique_id, model, ctask_dist, service_distribution)
+		super().__init__(unique_id, model)
+		self.unique_id = unique_id
 		self.ctask_status = np.random.choice(5, 1, ctask_dist)
-		self.er_service_time = ss.poisson(service_distribution[self.ctask_status]).rvs()
-		self.triage_entry_time = np.int(ss.beta(3, 3).rvs() * ticks) + 1
+		self.er_service_time = ss.poisson(service_distribution[int(self.ctask_status - 1)]).rvs()
+		self.triage_entry_time = np.int(ss.beta(3, 3).rvs() * self.model.ticks) + 1
 		self.triage_time = ss.poisson(3).rvs() + 1
 
 		self.triage_queue = False
@@ -32,16 +35,18 @@ class Patient(Agent):
 		return
 
 	def step(self):
-		if not self.triage and (self.model.current_tick >= self.triage_entry_time):
+		print("PATIENT{} WORKING".format(self.unique_id))
+		if not self.triaged and (self.model.current_tick >= self.triage_entry_time):
 			self.triage_queue = True
 
 		return
 
 
 class TriageNurses(Agent):
-	def __init__(self, model, n_nurses):
-		super().__init__(unique_id, model, n_nurses)
+	def __init__(self, unique_id, model, n_nurses):
+		super().__init__(unique_id, model)
 		self.entry_queue = []
+		self.n_nurses = n_nurses
 		self.n_busy = 0
 		self.patients_triaging = []
 
@@ -58,7 +63,7 @@ class TriageNurses(Agent):
 				self.entry_queue.insert(0)
 
 		# Release patients from nurses if time is up
-		for i in range(n_nurses):
+		for i in range(self.n_nurses):
 			patient, entry_time = self.patients_triaging[i]
 			if (self.model.current_tick - entry_time) >= patient.triage_time:
 
@@ -80,8 +85,9 @@ class TriageNurses(Agent):
 		return
 
 class Doctors(Agent):
-	def __init__(self, model, n_doctors):
-		super().__init__(unique_id, model, n_doctors)
+	def __init__(self, unique_id, model, n_doctors):
+		super().__init__(unique_id, model)
+		self.n_doctors = n_doctors
 		self.backlog = []
 		self.n_busy = 0
 		self.current_patients = []
@@ -101,7 +107,7 @@ class Doctors(Agent):
 		return
 
 	def step(self):
-		for i in range(n_doctors):
+		for i in range(self.n_doctors):
 			patient, entry_time = self.current_patients[i]
 			if (self.model.current_tick - entry_time) >= patient.service_time:
 				self.current_patients.pop(i)
@@ -114,26 +120,74 @@ class Doctors(Agent):
 		return
 
 class ERModel(Model):
-	def __init__(self, n_patients, n_triage_nurses, n_doctors, ticks, ctask_dist, service_distribution):
+	def __init__(self, n_patients, n_triage_nurses, n_doctors, ticks, ctask_dist, service_dist):
 		self.ticks = ticks
 		self.current_tick = 1
 		self.n_patients = n_patients
 		self.n_triage_nurses = n_triage_nurses
 		self.n_doctors = n_doctors
 		self.ctask_dist = ctask_dist
-		self.service_distribution = service_distribution
+		self.service_dist = service_dist
 
 
 		self.schedule = RandomActivation(self)
-		self.patients = [Patient(i, model, ctask_dist, service_distribution) for i in range(self.n_patients)]
-		self.triage_nurses = TriageNurses(model, self.n_triage_nurses)
-		self.doctors = Doctors(model, self.n_doctors)
+		self.patients = []
+		for i in range(self.n_patients):
+			patient = Patient(i, self, self.ctask_dist, self.service_dist)
+			self.schedule.add(patient)
+
+			self.patients.append(patient)
+
+		self.triage_nurses = TriageNurses(self.n_patients + 1, self, self.n_triage_nurses)
+		self.schedule.add(self.triage_nurses)
+
+		self.doctors = Doctors(self.n_patients + 2, self, self.n_doctors)
+		self.schedule.add(self.doctors)
 
 	def step(self):
+		print("STEP")
 		self.schedule.step()
 		self.current_tick += 1
 
 		return
+
+def main(args):
+	ctask_dist = [0.02, 0.25, 0.35, 0.28, 0.1]
+	service_dist = [80, 45, 35, 20, 15]
+	ticks = args.ticks
+	er_model = ERModel(args.n_patients, args.n_triage_nurses, args.n_doctors, ticks, ctask_dist, service_dist)
+
+	for i in range(ticks):
+		er_model.step()
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		'--n_patients',
+		default = 200,
+		type = int)
+
+	parser.add_argument(
+		'--n_triage_nurses',
+		default = 3,
+		type = int
+		)
+
+	parser.add_argument(
+		'--n_doctors',
+		default = 2,
+		type = int
+		)
+	parser.add_argument(
+		'--ticks',
+		default=500,
+		type=int
+		)
+	args = parser.parse_args()
+
+	main(args)
+
 
 
 
